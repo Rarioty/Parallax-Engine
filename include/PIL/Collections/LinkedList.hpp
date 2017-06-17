46e4f42909da905bb7b2687f3f085034a40c9de0
Rarioty <arthur.chevalier@inria.fr>
2017-06-17 17:01:33 +0200

    feat(collections): Adding LinkedList documentation and performances tests

--- include/PIL/Collections/LinkedList.hpp
+++ include/PIL/Collections/LinkedList.hpp
@@ -1,7 +1,8 @@
 #ifndef LINKED_LIST_HPP
 #define LINKED_LIST_HPP
 
-#include <PIL/Collections/Collections.hpp>
+#include <PIL/Types.hpp>
+#include <functional>
 #include <cstdlib>
 
 namespace Parallax
@@ -15,17 +16,37 @@ namespace Parallax
         template <typename T>
         class LinkedListIterator;
 
+        /**
+         * \class   LinkedList
+         * \brief   Single linked-list container, you can see performance below.
+         *
+         * \tparam  T   Type of the elements in the container.
+         *
+         * The performances source code are available in this file tests/PIL/Collections/perfs_collection_linked_list.cpp
+         * \code
+         *  | Test name   | Nb elements |    Std time   | Parallax time |
+         *  |-------------+-------------+---------------+---------------|
+         *  | push_front  | 1 000 000   |     0.19898 s |   0.0463889 s |
+         *  | push_back   | 1 000 000   |   0.0963715 s |   0.0271976 s |
+         *  | remove half | 10 000      |    0.647498 s |   0.0859716 s |
+         *  | iterate     | 1 000 000   |  0.00253338 s |  0.00266081 s |
+         * \endcode
+         */
         template <typename T>
-        class LinkedList : public Container<T>
+        class LinkedList
         {
         public:
-            using Iterator = LinkedListIterator<T>;
+            using Iterator = LinkedListIterator<T>;     /*!<    Iterator class for this container   */
             friend class LinkedListIterator<T>;
 
+            /**
+             * \struct  node
+             * \brief   Node in the linked-list
+             */
             struct node
             {
-                node*   m_next;
-                T       m_data;
+                node*   m_next;         /*!<    Next node           */
+                T       m_data;         /*!<    Data in this node   */
 
                 node(T data, node* next)
                     : m_next(next)
@@ -34,46 +55,197 @@ namespace Parallax
             };
 
         public:
+            /**
+             * \brief
+             *  Constructor
+             */
             LinkedList();
+            /**
+             * \brief
+             *  Destructor
+             */
             ~LinkedList();
 
         public:
+            /**
+             * \brief
+             *  Return an iterator corresponding at the start of this container
+             *
+             * \return  Iterator positioned at the beginning
+             */
             Iterator                begin();
+            /**
+             * \brief
+             *  Return an iterator corresponding at the end of this container
+             *
+             * \return  Iterator positioned at the end
+             */
             Iterator                end();
 
+            /**
+             * \brief
+             *  Push element in the front of this container
+             *
+             * \param[in]   element     Element to add
+             */
             void                    push_front(T element);
+            /**
+             * \brief
+             *  Push element in the back of this container
+             *
+             * \param[in]   element     Element to add
+             */
             void                    push_back(T element);
+            /**
+             * \brief
+             *  Push element in a specified position of this container
+             *
+             * \param[in]   element     Element to add
+             * \param[in]   position    Where to add the element
+             */
             void                    insert(T element, U32 position);
 
+            /**
+             * \brief
+             *  Return size of this container
+             *
+             * \return  Current size of this container
+             */
             U32                     size();
 
+            /**
+             * \brief
+             *  Remove a specified position in this container
+             *
+             * \param[in]   position    Position to remove
+             */
             void                    remove(U32 position);
+            /**
+             * \brief
+             *  Remove nodes in this container with a special function.
+             *  When the function return true then the node is removed otherwise the node remains.
+             *  The arguments sent to the function are the position of this node and the element inside this node.
+             *
+             * \param   func    Test function
+             */
             void                    remove(const std::function<bool(U32, T)>& func);
+            /**
+             * \brief
+             *  Remove the node described by an iterator
+             *
+             * \param[in]   it  Position of the node to remove
+             */
             void                    remove(Iterator it);
 
+            /**
+             * \brief
+             *  Return the value at a specified position
+             *
+             * \param[in]   position    Position to get
+             *
+             * \return  Found value at this position
+             */
             T                       get(U32 position);
+            /**
+             * \brief
+             *  Return the value in the front of the container
+             *
+             * \return  Found value
+             */
             T                       front();
+            /**
+             * \brief
+             *  Return the value in the back of the container
+             *
+             * \return  Found value
+             */
             T                       back();
 
+            /**
+             * \brief
+             *  Execute a function on each element of this container.
+             *  The arguments of the function are the position of the node and a pointer to the element
+             *
+             * \param[in]   func    Function to apply to each element
+             */
             void                    execute(const std::function<void(U32, T*)>& func);
+            /**
+             * \brief
+             *  Execute a function on each element of this container.
+             *  The arguments of the function are the position of the node and a pointer to the element.
+             *  The iteration of the nodes continue while the function return true.
+             *
+             * \param[in]   func    Function to apply to each element
+             */
             void                    execute(const std::function<bool(U32, T*)>& func);
 
+            /**
+             * \brief
+             *  Return a random position value
+             *
+             * \return  Random node value
+             */
             T                       random();
 
+            /**
+             * \brief
+             *  Return the position of a given value
+             *
+             * \param[in]   needle  Value to search
+             *
+             * \return     A pair composed of a boolean that indicates wether or not the value is found and the position of the value
+             */
             std::pair<bool, U32>    find(T needle);
+            /**
+             * \brief
+             *  Return the position of a given value from the back of the container
+             *
+             * \param[in]   needle  Value to search
+             *
+             * \return     A pair composed of a boolean that indicates wether or not the value is found and the position of the value
+             */
             std::pair<bool, U32>    find_back(T needle);
+            /**
+             * \brief
+             *  Return the position of all values corresponding to the given one
+             *
+             * \param[in]   needle  Value to search
+             *
+             * \return     A pair composed of a boolean that indicates wether or not the value is found and the position of all the values
+             */
             std::pair<bool, U32*>   find_all(T needle);
+            /**
+             * \brief
+             *  Return the position of values accepted by a specified function.
+             *  The arguments of the function are the current node position and a pointer to the element.
+             *
+             * \param[in]   func    Search function
+             *
+             * \return     A pair composed of a boolean that indicates wether or not the value is found and the position of the value
+             */
             std::pair<bool, U32*>   find_all(const std::function<bool(U32, T*)>& func);
 
+            /**
+             * \brief
+             *  Sort this container with a comparator function
+             *
+             * \param[in]   comparator  Function to compare two elements
+             */
             void                    sort(const std::function<bool(T*, T*)>& comparator);
 
         private:
-            struct node*    m_front;
-            struct node*    m_back;
+            struct node*    m_front;            /*!<    Front node of the container     */
+            struct node*    m_back;             /*!<    Back node of the container      */
 
-            U32             m_current_size;
+            U32             m_current_size;     /*!<    Current size of the container   */
         };
 
+        /**
+         * \class   LinkedListIterator
+         * \brief   Iterator for the LinkedList class
+         *
+         * \tparam  T   Type of the elements in the corresponding LinkedList
+         */
         template <typename T>
         class LinkedListIterator
         {
@@ -81,24 +253,66 @@ namespace Parallax
             friend class LinkedList<T>;
 
         public:
-            LinkedListIterator(U32 pos, LinkedList<T>* list);
+            /**
+             * \brief
+             *  Constructor
+             *
+             * \param[in]   node    Starting node for this iterator
+             */
+            LinkedListIterator(struct LinkedList<T>::node* node);
+            /**
+             * \brief
+             *  Constructor
+             */
             LinkedListIterator();
+            /**
+             * \brief
+             *  Destructor
+             */
             ~LinkedListIterator();
 
         public:
-            T                       operator*();
+            /**
+             * \brief
+             *  Access operator of this iterator
+             *
+             * \return  The value pointed by this iterator
+             */
+            T   operator*();
 
-            LinkedListIterator<T>   next();
+            /**
+             * \brief
+             *  Make a step with this iterator
+             */
+            void    next();
 
-            // Operators
-            LinkedListIterator<T>   operator++();
+            /**
+             * \brief
+             *  Make a step with this iterator. see next()
+             */
+            void    operator++();
 
-            bool                    operator!=(LinkedListIterator<T> it);
-            bool                    operator==(LinkedListIterator<T> it);
+            /**
+             * \brief
+             *  Comparator operator
+             *
+             * \param[in]   it  Other iterator to compare with
+             *
+             * \return  Whether or not the iterators are different
+             */
+            bool    operator!=(LinkedListIterator<T> it);
+            /**
+             * \brief
+             *  Comparator operator
+             *
+             * \param[in]   it  Other iterator to compare with
+             *
+             * \return  Whether or not the iterators are equals
+             */
+            bool    operator==(LinkedListIterator<T> it);
 
         private:
-            LinkedList<T>*  m_list;
-            U32             m_current;
+            struct LinkedList<T>::node* m_current_node;     /*!<    Node pointed by this iterator   */
         };
 
         #include "LinkedList.inc"
