4bd63b9492c3ded3b066a601064b3679eb90db0a
Rarioty <arthur.chevalier@inria.fr>
2017-06-18 17:10:24 +0200

    fix(collections): Fixing error in vector and adding some tests

--- tests/PIL/Collections/test_collection_vector_iterator.cpp
+++ tests/PIL/Collections/test_collection_vector_iterator.cpp
@@ -8,6 +8,7 @@ using namespace Parallax;
 int main(int argc, char* argv[])
 {
     Collections::Vector<int> vector;
+    int ret;
 
     vector.push_back(0);
     vector.push_back(1);
@@ -31,6 +32,17 @@ int main(int argc, char* argv[])
     std::cout << std::endl;
 
     register_test(it == vector.end(), "Successfully ended loop");
+
+    ret = 0;
+    try {
+        it.next();
+        it.next();
+        *it;
+    } catch (std::runtime_error e) {
+        ret = 1;
+    }
+    register_test(ret == 1, "Successfully raised an error when accessing iterator too far");
+
 
     return end_test();
 }
