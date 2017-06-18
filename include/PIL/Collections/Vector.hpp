3b8952b9460bfb61e55cdb82e02c1854bf7ef2b8
Rarioty <arthur.chevalier@inria.fr>
2017-06-18 17:06:13 +0200

    feat(collections): Added stack and documentation for vector

--- include/PIL/Collections/Vector.hpp
+++ include/PIL/Collections/Vector.hpp
@@ -4,7 +4,6 @@
 #include <PIL/Types.hpp>
 #include <functional>
 #include <cstring>
-#include <iostream>
 
 namespace Parallax
 {
@@ -31,48 +30,213 @@ namespace Parallax
             friend class VectorIterator<T>;
 
         public:
+            /**
+             * \brief
+             *  Constructor
+             *
+             * \param[in]   start_size  Default starting size to the container
+             */
             Vector(U32 start_size = 32);
+            /**
+             * \brief
+             *  Destructor
+             */
             ~Vector();
 
         public:
+            /**
+             * \brief
+             *  Return an iterator corresponding at the start of this container
+             *
+             * \return  Iterator positioned at the beginning
+             */
             Iterator                begin();
+            /**
+             * \brief
+             *  Return an iterator corresponding at the end of this container
+             *
+             * \return  Iterator positioned at the end
+             */
             Iterator                end();
 
+            /**
+             * \brief
+             *  Push element in the front of this container
+             *
+             * \param[in]   element     Element to add
+             */
             void                    push_front(T element);
+            /**
+             * \brief
+             *  Push element in the back of this container
+             *
+             * \param[in]   element     Element to add
+             */
             void                    push_back(T element);
+            /**
+             * \brief
+             *  Push element in a specified position of this container
+             *
+             * \param[in]   element     Element to add
+             * \param[in]   position    Where to add the element
+             */
             void                    insert(T element, U32 position);
 
+            /**
+             * \brief
+             *  Pop an element from the back of the vector
+             */
             void                    pop_back();
 
+            /**
+             * \brief
+             *  Return size of this container
+             *
+             * \return  Current size of this container
+             */
             U32                     size();
+            /**
+             * \brief
+             *  Resize this vector with a specified size
+             *
+             * \param[in]   new_size    New size for the container
+             */
             void                    resize(U32 new_size);
 
+            /**
+             * \brief
+             *  Remove a specified position in this container
+             *
+             * \param[in]   position    Position to remove
+             */
             void                    remove(U32 position);
+            /**
+             * \brief
+             *  Remove nodes in this container with a special function.
+             *  When the function return true then the node is removed otherwise the node remains.
+             *  The arguments sent to the function are the position of this node and the element inside this node.
+             *
+             * \param   func    Test function
+             */
             void                    remove(const std::function<bool(U32, T)>& func);
+            /**
+             * \brief
+             *  Remove the node described by an iterator
+             *
+             * \param[in]   it  Position of the node to remove
+             */
             void                    remove(Iterator it);
 
+            /**
+             * \brief
+             *  Return the value at a specified position
+             *
+             * \param[in]   position    Position to get
+             *
+             * \return  Found value at this position
+             */
             T                       get(U32 position);
+            /**
+             * \brief
+             *  Return the value in the front of the container
+             *
+             * \return  Found value
+             */
             T                       front();
+            /**
+             * \brief
+             *  Return the value in the back of the container
+             *
+             * \return  Found value
+             */
             T                       back();
 
+            /**
+             * \brief
+             *  Get a reference to the value in a specified position
+             *
+             * \param[in]   position    Position of the wanted value
+             *
+             * \return  Reference to the value
+             */
             T&                      at(U32 position);
 
+            /**
+             * \brief
+             *  Execute a function on each element of this container.
+             *  The arguments of the function are the position of the node and a pointer to the element
+             *
+             * \param[in]   func    Function to apply to each element
+             */
             void                    execute(const std::function<void(U32, T*)>& func);
+            /**
+             * \brief
+             *  Execute a function on each element of this container.
+             *  The arguments of the function are the position of the node and a pointer to the element.
+             *  The iteration of the nodes continue while the function return true.
+             *
+             * \param[in]   func    Function to apply to each element
+             */
             void                    execute(const std::function<bool(U32, T*)>& func);
 
+            /**
+             * \brief
+             *  Return a random position value
+             *
+             * \return  Random node value
+             */
             T                       random();
 
+            /**
+             * \brief
+             *  Return the position of a given value
+             *
+             * \param[in]   needle  Value to search
+             *
+             * \return     A pair composed of a boolean that indicates wether or not the value is found and the position of the value
+             */
             std::pair<bool, U32>    find(T needle);
+            /**
+             * \brief
+             *  Return the position of a given value from the back of the container
+             *
+             * \param[in]   needle  Value to search
+             *
+             * \return     A pair composed of a boolean that indicates wether or not the value is found and the position of the value
+             */
             std::pair<bool, U32>    find_back(T needle);
-            std::pair<U32, U32*>    find_all(T needle);
-            std::pair<U32, U32*>    find_all(const std::function<bool(U32, T*)>& func);
+            /**
+             * \brief
+             *  Return the position of all values corresponding to the given one
+             *
+             * \param[in]   needle  Value to search
+             *
+             * \return     A pair composed of a boolean that indicates wether or not the value is found and the position of all the values
+             */
+            std::pair<U32, U32*>     find_all(T needle);
+            /**
+             * \brief
+             *  Return the position of values accepted by a specified function.
+             *  The arguments of the function are the current node position and a pointer to the element.
+             *
+             * \param[in]   func    Search function
+             *
+             * \return     A pair composed of a boolean that indicates wether or not the value is found and the position of the value
+             */
+            std::pair<U32, U32*>     find_all(const std::function<bool(U32, T*)>& func);
 
+            /**
+             * \brief
+             *  Sort this container with a comparator function
+             *
+             * \param[in]   comparator  Function to compare two elements
+             */
             void                    sort(const std::function<bool(T, T)>& comparator);
 
         private:
-            U32     m_max_size;
-            U32     m_current;
-            T*      m_array;
+            U32     m_max_size;     /*!<    Size of the allocated array     */
+            U32     m_current;      /*!<    Current size used by the vector */
+            T*      m_array;        /*!<    Array of elements               */
         };
 
         /**
---
+++ tests/PIL/Collections/test_collection_stack.cpp
@@ -0,0 +1,66 @@
+#include <PIL/Collections/Stack.hpp>
+
+#include <tests.hpp>
+
+using namespace Parallax;
+
+int main(int argc, char* argv[])
+{
+    Collections::Stack<int>* stack;
+    int ret, i;
+
+    ret = 0;
+    try {
+        stack = new Collections::Stack<int>(0);
+    } catch (std::runtime_error e) {
+        ret = 1;
+    }
+    register_test(ret == 1, "Successfully raised an error when trying to create a zero element stack");
+
+    stack = new Collections::Stack<int>();
+
+    ret = 0;
+    try {
+        stack->top();
+    } catch (std::runtime_error e) {
+        ret = 1;
+    }
+    register_test(ret == 1, "Successfully raised an error when trying to get an element from an empty stack");
+
+    ret = 0;
+    try {
+        stack->pop();
+    } catch (std::runtime_error e) {
+        ret = 1;
+    }
+    register_test(ret == 1, "Successfully raised an error when trying to pop an empty stack");
+
+    register_test(stack->empty(), "Stack empty");
+
+    stack->push(0);
+    stack->push(1);
+    stack->push(2);
+
+    register_test(!stack->empty(), "Stack non empty");
+    register_test(stack->size() == 3, "Correct size for the stack");
+
+    register_test(stack->top() == 2, "Correct element on the top");
+
+    register_test(stack->pop() == 2, "Correct value returned when pop");
+
+    register_test(stack->top() == 1, "Correct element on the top after pop");
+
+    stack->pop();
+    stack->pop();
+
+    for (i = 0; i < 60; ++i)
+    {
+        stack->push(i);
+    }
+
+    register_test(stack->size() == 60, "Correct size after array resizing");
+
+    delete stack;
+
+    return end_test();
+}
