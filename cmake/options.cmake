da667cf342b113063568d1ce914715213adf0b33
Rarioty <arthur.chevalier@inria.fr>
2017-07-15 16:44:42 +0200

    feat(examples): Adding examples

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,3 +1,4 @@
 # Add all options for cmake
 option(BUILD_DOCUMENTATION  "Create and install the HTML based API documentation (requires Doxygen)" ${DOXYGEN_FOUND})
 option(BUILD_TESTS          "Build and add a make command to launch tests" true)
+option(BUILD_EXAMPLES       "Build examples" true)
