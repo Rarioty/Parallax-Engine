18d585832a1dca1fb71bb03f5699b13cd7efa8fa
Rarioty <arthur.chevalier@inria.fr>
2017-08-06 13:16:04 +0200

    feat(Core): Adding base IDs management and Maths hash functions

---
+++ include/Parallax/ID.hpp
@@ -0,0 +1,15 @@
+#ifndef PARALLAX_ID_HPP_GUARD
+#define PARALLAX_ID_HPP_GUARD
+
+#include <Parallax/Types.hpp>
+#include <string>
+
+namespace Parallax
+{
+    namespace ID
+    {
+        U32 generateID(const char* name);
+    }
+}
+
+#endif
---
+++ include/Parallax/Maths/Hash.hpp
@@ -0,0 +1,15 @@
+#ifndef PARALLAX_HASH_HPP_GUARD
+#define PARALLAX_HASH_HPP_GUARD
+
+#include <Parallax/Types.hpp>
+#include <string>
+
+namespace Parallax
+{
+    namespace Maths
+    {
+        U32 crc32(const char* name, U32 len);
+    }
+}
+
+#endif
